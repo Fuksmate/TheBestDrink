f72d68e2043d7a080da705c9e13b71ecab00c9ab
Łukasz Fuchs <lukaszfuchs18@gmail.com>
2020-09-01 16:33:31 +0200

    project configuration

---
+++ components/TheHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="row header">
+    <div class="columns small-12 text-center header__logo">
+        This is the database of the best drinks of all time
+    </div>
+    <div class="columns small-12 header__beam">
+      <nav class="columns large-8 large-offset-3 text">
+            <ul>
+                <li><nuxt-link to="/about">Home</nuxt-link></li>
+                <li><nuxt-link to="/about">About</nuxt-link></li>
+                <li><nuxt-link to="/about">RNG Drink</nuxt-link></li>
+                <li><nuxt-link to="/about">Top 10</nuxt-link></li>
+            </ul>
+      </nav> 
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.header {
+    &__logo{
+        height: 5vh;
+        background-color: #F2E9E1;
+        font-size:22px
+    }
+    &__beam{
+        background-color: #CBE86B;
+        height: 10vh;
+        position: relative;
+        ul {
+            list-style-type: none;
+            position: absolute;
+            top:50%;
+            transform: translate(0, -50%);
+            li {
+                display: block;
+                float: left;
+                text-align: center;
+                font-size:17px;
+                font-weight: 200;
+                width: 230px;
+            }
+        }
+    }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TheHeader />
+    <Nuxt />
+  </div>
+</template>
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    TheHeader
+  }
+}
+</script>
+<style lang="scss">
+</style>
