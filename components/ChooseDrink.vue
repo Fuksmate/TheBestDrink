48dbea995cedb20ede2a840bbe38d780ada34a40
Łukasz Fuchs <lukaszfuchs18@gmail.com>
2020-09-01 18:21:33 +0200

    adding selection buttons

--- components/ChooseDrink.vue
+++ components/ChooseDrink.vue
@@ -1,9 +1,10 @@
 <template>
     <main class="row choose-drink">
-        <div class="columns small-12 medium-4 medium-offset-4">
+        <div class="columns small-12 medium-4 medium-offset-4 choose-drink__relative">
             <img :src="drink" alt="drink">
-            <div class="fullwidth">
-           
+            <div class="choose-drink__circle choose-drink__circle--left">
+            </div>
+            <div class="choose-drink__circle choose-drink__circle--right">
             </div>
         </div>
     </main>
@@ -17,12 +18,32 @@ export default {
             type: String,
             required: true,
         }
-    }
+    },
 }
 </script>
 
 <style lang="scss">
 .choose-drink {
     padding-top: 20px;
+    &__relative {
+        position: relative;
+    }
+    &__circle {
+        border-radius: 50%;
+        width: 125px;
+        height: 125px;
+        &--left {
+            position: absolute;
+            background-color: red;
+            bottom: 0;
+            left: 18px;
+        }
+        &--right {
+            position: absolute;
+            background-color: greenyellow;
+            bottom: 0;
+            right: 18px;
+        }
+    }
 }
 </style>
